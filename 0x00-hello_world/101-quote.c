d977b6b1ee25542d01b51977094dc8abcd6ae30e
mdjibo2 <justpourmestest00@gmail.com>
2022-06-30 12:53:17 -0700

    print sentence to  to the standard error without put and printf function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
  * main - print sentence to the standard error
  *
  * Return: Always 1 (Success)
  */
-int main()
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1r\n", 115);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
