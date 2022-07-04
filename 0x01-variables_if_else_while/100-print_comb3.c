4e1bb13087beeee349dd403159dc554000c12ba2
mdjibo2 <justpourmestest00@gmail.com>
2022-07-04 11:28:43 -0700

    2 digits combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	int i;
+
+	for(i = 0; i < 100 ; i++)
+	
+	{	
+		putchar((i/10)+'0');
+		putchar((i%10)+'0');
+		if (i < 100)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return 0;
+}
