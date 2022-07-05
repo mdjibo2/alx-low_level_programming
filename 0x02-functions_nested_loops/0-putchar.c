e7b10a714d9d9deec6661cbf394d6c3398e98979
mdjibo2 <justpourmestest00@gmail.com>
2022-07-05 11:48:37 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *main - Print string without using printf
+ *
+ *Description: Print the string _putchar using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
