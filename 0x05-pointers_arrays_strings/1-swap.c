218c144064bf1b9c1c59772ff039de879d3ce044
mdjibo2 <justpourmestest00@gmail.com>
2022-07-12 03:59:15 -0700

    swap pointer value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @a: pointer to an integer
+ * @b: pointer to an integer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
