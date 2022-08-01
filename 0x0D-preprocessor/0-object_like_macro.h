479e005118ca40f1060c491c6fc1c2dbce4c62c4
mdjibo2 <justpourmestest00@gmail.com>
2022-08-01 15:05:52 -0700

    Correction

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
 /**
+ * File: 0-object_like_macro.h
  * Description: Header file that defines a macro name
- *   SIZE as an abbreviation for the token 1024.
+ * SIZE as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
+
+#endif
