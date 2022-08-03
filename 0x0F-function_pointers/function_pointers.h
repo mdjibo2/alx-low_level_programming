6661093d3abea53c78185b3b816983e89b6e82b2
mdjibo2 <justpourmestest00@gmail.com>
2022-08-03 15:23:17 -0700

    correction

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
+/*
  * Description: Header file containing prototypes for all functions
  */
 
@@ -10,7 +10,5 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int _putchar(char c);
 
 #endif
