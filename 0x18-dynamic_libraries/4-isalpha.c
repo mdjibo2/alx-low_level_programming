f292ee474d69155d1f066bceb630a7dc91dd8ec7
mdjibo2 <justpourmestest00@gmail.com>
2022-10-03 18:13:01 +0000

    Create the dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - check for alphabet.
+ * @c: variable
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
